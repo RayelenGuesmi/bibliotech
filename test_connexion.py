cae91a78dc99be084213f2630c62fc1369622478
Guesmi Ghaylen <rayelen8010@gmail.com>
2026-09-17 14:05:41 +0200

    Retrait du script de test temporaire

--- test_connexion.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import text
-from app.database import engine
-
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT 'Connexion OK depuis Python' FROM dual"))
-    print(result.scalar())
